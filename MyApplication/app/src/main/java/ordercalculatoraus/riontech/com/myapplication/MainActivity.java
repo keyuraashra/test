532e85b7f1ff137cc1cc00d50b9db40e49bfcd75
Keyur Ashra <keyuraashra@gmail.com>
2015-10-01 14:43:25 +0530

    keyur code

--- MyApplication/app/src/main/java/ordercalculatoraus/riontech/com/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/ordercalculatoraus/riontech/com/myapplication/MainActivity.java
@@ -33,6 +33,7 @@ public class MainActivity extends ActionBarActivity {
         if (id == R.id.action_settings) {
             return true;
         } else {
+			"keyur";
 		}
 
         return super.onOptionsItemSelected(item);
